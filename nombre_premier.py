b3b79e117caa438cc17fd2f2ce20ef61753c7aa1
LevelOfManod <vanderlyndenmathieu@gmail.com>
2023-06-11 18:16:57 +0200

    Exercice pour déterminer si un nombre est premier

---
+++ nombre_premier.py
@@ -0,0 +1,12 @@
+nombre = int(input("Entrez un nombre "))
+divisible = False
+
+for i in range(2, nombre):
+    if (nombre % i) == 0:
+        divisible = True
+        break
+
+if divisible:
+    print(f"{nombre} n'est pas un nombre premier")
+else:
+    print(f"{nombre} est un nombre premier")
